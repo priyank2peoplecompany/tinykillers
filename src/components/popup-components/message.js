12d852013a263c4e6113fee9b65e379fb36515e4
yash-bytes1 <yash.sanja@bytestechnolab.in>
2021-12-25 18:07:54 +0530

    reversse clock implemented need to add api date

--- src/components/popup-components/message.js
+++ src/components/popup-components/message.js
@@ -1,9 +1,69 @@
 import React from "react";
+import { useState, useEffect } from 'react';
 import tinyLogo from "../../assets/images/TinyLogo.svg";
 import './startQuiz.css'
 import samurai from '../../assets/images/Samurai_Pose03_04.png'
-
+let time = new Date().toLocaleString();
 const Message = (props) => {
+
+    const [seconds, setSeconds] = useState(59);
+    const [minutes, setMinutes] = useState(59);
+    const [hours, setHours] = useState(8);
+  
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    function updateTime() {
+      localStorage.setItem('lastTIme', `${hours}:${minutes}:${seconds}`);
+      if (minutes === 0 && seconds === 0) {
+        setSeconds(0);
+        setMinutes(0);
+        setHours(9);
+      } else {
+        if (hours === 0) {
+          if(minutes === 0 && seconds === 0){
+            console.log('stop watch');
+          }
+        } else {
+          if (minutes === 0) {
+            setMinutes(59);
+            setHours((hours) => hours - 1);
+            setSeconds(59);
+          } else {
+            if (seconds === 0) {
+              setMinutes((minutes) => minutes - 1);
+              setSeconds(59);
+            } else {
+              setSeconds((seconds) => seconds - 1);
+            }
+          }
+        }
+      }
+    }
+
+    useEffect(() => {
+        const token = setTimeout(updateTime, 1000);
+    
+        return function cleanUp() {
+          clearTimeout(token);
+        };
+      },[updateTime]);
+      useEffect(() => {
+        var date = new Date();
+        const date24hours = date.toLocaleString('en-US',{hour12:false})
+        localStorage.setItem('currentTIme', date.toLocaleString('en-US',{hour12:false}));
+        const currentHour = new Date(date24hours).getHours()
+        const currentMinutes = new Date(date24hours).getMinutes()
+        const currentSeconds = new Date(date24hours).getSeconds()
+        // setMinutes(new Date(date24hours).getMinutes() - currentMinutes);
+        // setHours(new Date(date24hours).getHours() - currentHour);
+        // setSeconds(new Date(date24hours).getSeconds() - currentSeconds);
+        setMinutes(2);
+        setHours(4);
+        setSeconds(34);
+        
+      },[]);
+    
+
+
     return (
         <>
             <div className="container text-center">
@@ -18,7 +78,7 @@ const Message = (props) => {
                         <div className="w-100">
                             <div className="event mt-5">
                                 <span>Event starts in:</span>
-                                <h5 className="mt-2">06 HOURS, 11 MINUTES, 53 SECONDS</h5>
+                                <h5 className="mt-2">{hours} HOURS, {minutes} MINUTES, {seconds < 10 ?  `0${seconds}` : seconds} SECONDS</h5>
                             </div>
                             <button className="btn start-btn mt-4">MINT</button>
                         </div>
