b790377e4358a038bfb8e77873cca1b631a7e1ed
priyanktwopeoplecompany <priyank@twopeople.company>
2022-01-06 14:12:32 +0530

    Add user address

--- truffle-config.js
+++ truffle-config.js
@@ -46,6 +46,7 @@ module.exports = {
       host: "127.0.0.1", // Localhost (default: none)
       port: 8545, // Standard Ethereum port (default: none)
       network_id: "*", // Any network (default: none)
+      from: '0x167f56ef180d0A8771143E294cbDA67741f3edFc',
     },
     // Another network with more advanced options...
     // advanced: {
