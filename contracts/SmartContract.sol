fa9163762c14dd62d925b53e19808e7daea69795
waris <waris@twopeople.company>
2022-01-20 10:52:59 +0530

    deploy contract

--- contracts/SmartContract.sol
+++ contracts/SmartContract.sol
@@ -1289,112 +1289,92 @@ contract Whitelist is Ownable {
 
 }
 
-contract SmartContract is ERC721Enumerable, Whitelist {
-  using Strings for uint256;
+contract TinyKiller is ERC721Enumerable, Whitelist {
+    using Strings for uint256;
+    string public baseURI;
+    string public baseExtension = ".json";
+    uint256 public cost = 0.01 ether;
+    uint256 public mintPerTransactionAllowed = 20;
+    bool public paused = false;
+    struct tinyTypeSupply { uint16 start; uint16 end;} 
+    mapping(string => tinyTypeSupply) types;
+    
+    constructor(string memory _name , string memory _symbol, string memory _URI) ERC721(_name,_symbol) {
+        types["ASAGO"].start = 1;
+        types["ASAGO"].end = 2000;
+        types["KAJIWARA"].start = 2001;
+        types["KAJIWARA"].end = 4000;
+        types["KUSAKI"].start = 4001;
+        types["KUSAKI"].end = 6000;
+        types["SAKEDA"].start = 6001;
+        types["SAKEDA"].end = 8000;
+        types["TOZAWA"].start = 8001;
+        types["TOZAWA"].end = 9999;
+        setBaseURI(_URI);
+    }
 
-  string public baseURI;
-  string public baseExtension = ".json";
-  uint256 public cost = 100 ether;
-  uint256 public maxSupply = 1000;
-  uint256 public maxMintAmount = 20;
-  bool public paused = false;
-  mapping(address => bool) public whitelisted;
+    // internal
+    function _baseURI() internal view virtual override returns (string memory) {
+        return baseURI;
+    }
 
-  constructor(
-    string memory _name,
-    string memory _symbol,
-    string memory _initBaseURI
-  ) ERC721(_name, _symbol) {
-    setBaseURI(_initBaseURI);
-    mint(msg.sender, 5);
-  }
-
-  // internal
-  function _baseURI() internal view virtual override returns (string memory) {
-    return baseURI;
-  }
-
-  // public
-  function mint(address _to, uint256 _mintAmount) public payable {
-    uint256 supply = totalSupply();
-    require(!paused);
-    require(_mintAmount > 0);
-    require(_mintAmount <= maxMintAmount);
-    require(supply + _mintAmount <= maxSupply);
-
-    if (msg.sender != owner()) {
-        if(whitelisted[msg.sender] != true) {
-          require(msg.value >= cost * _mintAmount);
+    /**
+    * @dev payable function to mint nfts by killer type
+    * @param _to, _mintAmount, _killerType
+    */
+    function mint(address _to, uint256 _mintAmount,string memory _killerType) public payable {
+        require(!paused,"Minting is paused");
+        require(_mintAmount > 0, "Amount should be greater than zero");
+        require(_mintAmount <= mintPerTransactionAllowed, "You can mint a maximum of 20 NFT at once");
+        require(types[_killerType].start + _mintAmount <= types[_killerType].end, "Exceeds maximum supply");
+        require(msg.value >= cost * _mintAmount , "Ether sent is not correct");
+        for (uint256 i = 1; i <= _mintAmount; i++) {
+            _safeMint(_to, types[_killerType].start);
+            types[_killerType].start += 1;
         }
     }
-    
-    for (uint256 i = 1; i <= _mintAmount; i++) {
-      _safeMint(_to, supply + i);
+
+    function remainingSupplyByType(string memory _type) public view returns(uint16){
+       return (types[_type].end - types[_type].start);
     }
-  }
 
-  function walletOfOwner(address _owner)
-    public
-    view
-    returns (uint256[] memory)
-  {
-    uint256 ownerTokenCount = balanceOf(_owner);
-    uint256[] memory tokenIds = new uint256[](ownerTokenCount);
-    for (uint256 i; i < ownerTokenCount; i++) {
-      tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+    function walletOfOwner(address _owner) public view returns (uint256[] memory) {
+        uint256 ownerTokenCount = balanceOf(_owner);
+        uint256[] memory tokenIds = new uint256[](ownerTokenCount);
+        for (uint256 i; i < ownerTokenCount; i++) {
+            tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+        }
+        return tokenIds;
     }
-    return tokenIds;
-  }
 
-  function tokenURI(uint256 tokenId)
-    public
-    view
-    virtual
-    override
-    returns (string memory)
-  {
-    require(
-      _exists(tokenId),
-      "ERC721Metadata: URI query for nonexistent token"
-    );
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        require(_exists(tokenId), "ERC721Metadata: URI query for nonexistent token" );
+        string memory currentBaseURI = _baseURI();
+        return bytes(currentBaseURI).length > 0  ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension)) : "";
+    }
 
-    string memory currentBaseURI = _baseURI();
-    return bytes(currentBaseURI).length > 0
-        ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension))
-        : "";
-  }
+    //only owner
+    function setCost(uint256 _newCost) public onlyOwner() {
+        cost = _newCost;
+    }
 
-  //only owner
-  function setCost(uint256 _newCost) public onlyOwner() {
-    cost = _newCost;
-  }
+    function setmaxMintAmount(uint256 _mintPerTransactionAllowed) public onlyOwner() {
+        mintPerTransactionAllowed = _mintPerTransactionAllowed;
+    }
 
-  function setmaxMintAmount(uint256 _newmaxMintAmount) public onlyOwner() {
-    maxMintAmount = _newmaxMintAmount;
-  }
+    function setBaseURI(string memory _newBaseURI) public onlyOwner {
+        baseURI = _newBaseURI;
+    }
 
-  function setBaseURI(string memory _newBaseURI) public onlyOwner {
-    baseURI = _newBaseURI;
-  }
+    function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
+        baseExtension = _newBaseExtension;
+    }
 
-  function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
-    baseExtension = _newBaseExtension;
-  }
+    function pause(bool _state) public onlyOwner {
+        paused = _state;
+    }
 
-  function pause(bool _state) public onlyOwner {
-    paused = _state;
-  }
- 
-  function whitelistUser(address _user) public onlyOwner {
-    whitelisted[_user] = true;
-  }
- 
-  function removeWhitelistUser(address _user) public onlyOwner {
-    whitelisted[_user] = false;
-  }
-
-  function withdraw() public payable onlyOwner {
-    require(payable(msg.sender).send(address(this).balance));
-  }
-  
+    function withdraw() public payable onlyOwner {
+        require(payable(msg.sender).send(address(this).balance));
+    }  
 }
